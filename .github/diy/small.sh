42cf506f636a3950c3649cd96d86421b6695c274
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2024-12-17 21:21:40 +0800

    Update small.sh

--- .github/diy/small.sh
+++ .github/diy/small.sh
@@ -3,41 +3,25 @@ function mvdir() {
 mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
-##############################################################################################################################################
-##############################################################################################################################################
-##############################################################################################################################################
-#################################################弃用##########################################################################################
-########################################luci-app-chinadns-ng#############################################
-#原作者
-# git clone -b luci https://github.com/moruiris/openwrt-chinadns-ng ./luci-app-chinadns-ng
-# git clone https://github.com/pexcn/openwrt-chinadns-ng
-##############################################################################################################################################
-#################################################软件##########################################################################################
-########################################luci-app-smartdns#############################################
-#原作者
-#git clone -b lede https://github.com/pymumu/luci-app-smartdns
-#git clone https://github.com/pymumu/openwrt-smartdns
-#二次修改
-git clone https://github.com/kenzok8/openwrt-packages && mv openwrt-packages/luci-app-smartdns ./ && mv openwrt-packages/smartdns ./openwrt-smartdns && rm -rf openwrt-packages
-########################################luci-app-adguardhome##########################################
-#原作者
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome
-#二次修改
-git clone https://github.com/kongfl888/luci-app-adguardhome
 ########################################luci-app-passwall#############################################
 git clone https://github.com/xiaorouji/openwrt-passwall-packages ./openwrt-passwall
-#rm -rf ./openwrt-passwall/dns2socks
-#rm -rf ./openwrt-passwall/ipt2socks
-#rm -rf ./openwrt-passwall/microsocks
-#rm -rf ./openwrt-passwall/pdnsd-alt
+rm -rf ./openwrt-passwall/shadowsocks-rust
+rm -rf ./openwrt-passwall/v2ray-geodata
+rm -rf ./openwrt-passwall/chinadns-ng
+rm -rf ./openwrt-passwall/gn
 #原作者 第1版luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall ./passwall && mv passwall/luci-app-passwall ./ && rm -rf passwall
+git clone https://github.com/xiaorouji/openwrt-passwall ./openwrt-passwall && mv passwall/luci-app-passwall ./ && rm -rf passwall
 #原作者 第2版luci-app-passwall2
-git clone https://github.com/xiaorouji/openwrt-passwall2 ./passwall && mv passwall/luci-app-passwall2 ./ && rm -rf passwall
+git clone https://github.com/xiaorouji/openwrt-passwall2 ./openwrt-passwall && mv passwall/luci-app-passwall2 ./ && rm -rf passwall
 ########################################luci-app-ssr-plus#############################################
 #原作者
-git clone https://github.com/fw876/helloworld ./openwrt-ssr-plus && mv openwrt-ssr-plus/luci-app-ssr-plus ./
+git clone https://github.com/fw876/helloworld ./helloworld && mv openwrt-passwall/luci-app-ssr-plus ./ && rm -rf helloworld
 ############################################################################################################################################################
+
+########################################luci-app-lucky#############################################
+git clone https://github.com/gdy666/luci-app-lucky ./
+############################################################################################################################################################
+
 #################################################主题########################################################################################################
 ########################################luci-app-argon#############################################
 #原作者
