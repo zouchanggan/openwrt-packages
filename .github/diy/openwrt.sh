b1a4a972d7a26d2cf7f77b2c1f38fee3cf965ce3
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2024-12-19 22:02:04 +0800

    Update openwrt.sh

--- .github/diy/openwrt.sh
+++ .github/diy/openwrt.sh
@@ -3,30 +3,58 @@ function mvdir() {
 mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
-############################################################################################################################################################
-#################################################弃用##########################################################################################
-########################################luci-app-smartdns#############################################
-#原作者 openwrt已有
-#git clone https://github.com/pymumu/luci-app-smartdns
-#git clone https://github.com/pymumu/openwrt-smartdns
-#二次修改
-git clone https://github.com/kenzok8/openwrt-packages && mv openwrt-packages/luci-app-smartdns ./ && mv openwrt-packages/smartdns ./openwrt-smartdns && rm -rf openwrt-packages
-############################################################################################################################################################
-#################################################软件##########################################################################################
-########################################luci-app-adguardhome##########################################
+########################################luci-app-passwall#############################################
+git clone https://github.com/xiaorouji/openwrt-passwall-packages ./
+rm -rf ./openwrt-passwall/shadowsocks-rust
+rm -rf ./openwrt-passwall/v2ray-geodata
+rm -rf ./openwrt-passwall/chinadns-ng
+rm -rf ./openwrt-passwall/gn
+#原作者 第1版luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall ./passwall && mv passwall/luci-app-passwall ./ && rm -rf passwall
+#原作者 第2版luci-app-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall2 ./passwall2 && mv passwall2/luci-app-passwall2 ./ && rm -rf passwall2
+########################################luci-app-ssr-plus#############################################
 #原作者
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome
-#二次修改
-git clone https://github.com/kongfl888/luci-app-adguardhome
-########################################luci-app-chinadns-ng#############################################
-#原作者
-#git clone -b luci https://github.com/moruiris/openwrt-chinadns-ng ./luci-app-chinadns-ng
-#git clone https://github.com/pexcn/openwrt-chinadns-ng
+git clone https://github.com/fw876/helloworld ./helloworld && mv helloworld/luci-app-ssr-plus ./ && mv helloworld/shadowsocks-libev ./ && rm -rf helloworld
 ############################################################################################################################################################
-#################################################主题########################################################################################################
-git clone https://github.com/jerrykuku/luci-theme-argon.git
-####################################################################################
 
+########################################SSRP#############################################
+git clone https://github.com/zouchanggan/SSRP ./SSRP && mv SSRP/update/v2raya ./ && mv SSRP/themes//luci-theme-design ./ && rm -rf SSRP
+############################################################################################################################################################
+
+########################################small#############################################
+git clone https://github.com/kenzok8/small ./small 
+mv small/luci-app-homeproxy ./ 
+mv small/luci-app-mihomo ./ 
+mv small/luci-app-mosdns ./ 
+mv small/luci-app-openclash ./ 
+mv small/shadowsocks-rust ./
+mv small/mosdns ./ 
+mv small/v2ray-geodata ./
+mv small/chinadns-ng ./
+mv small/v2dat ./
+mv small/lua-neturl ./
+mv small/redsocks2 ./
+mv small/shadow-tls ./
+mv small/gn ./
+mv small/mihomo ./
+rm -rf small
+############################################################################################################################################################
+
+########################################luci-app-lucky#############################################
+git clone https://github.com/gdy666/luci-app-lucky ./luci-app-lucky && rm -rf luci-app-lucky/previews
+############################################################################################################################################################
+
+########################################luci-app-oaf#############################################
+git clone https://github.com/destan19/OpenAppFilter ./luci-app-oaf
+############################################################################################################################################################
+
+#################################################主题########################################################################################################
+########################################luci-app-argon#############################################
+#原作者
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config
+####################################################################################
 
 
 rm -rf ./*/.git & rm -f ./*/.gitattributes
