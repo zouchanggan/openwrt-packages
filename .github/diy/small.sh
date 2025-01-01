ee1cdde1ce56452018b011b83899821eb9476126
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2025-01-01 21:50:35 +0800

    Update small.sh

--- .github/diy/small.sh
+++ .github/diy/small.sh
@@ -9,17 +9,19 @@ rm -rf ./openwrt-passwall/shadowsocks-rust
 rm -rf ./openwrt-passwall/v2ray-geodata
 rm -rf ./openwrt-passwall/chinadns-ng
 rm -rf ./openwrt-passwall/gn
+cp -r openwrt-passwall/* ./ && rm -rf ./openwrt-passwall
 #原作者 第1版luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall ./passwall && mv passwall/luci-app-passwall ./ && rm -rf passwall
 #原作者 第2版luci-app-passwall2
 git clone https://github.com/xiaorouji/openwrt-passwall2 ./passwall2 && mv passwall2/luci-app-passwall2 ./ && rm -rf passwall2
 ########################################luci-app-ssr-plus#############################################
 #原作者
-git clone https://github.com/fw876/helloworld ./helloworld && mv helloworld/luci-app-ssr-plus ./ && mv helloworld/shadowsocks-libev ./openwrt-passwall && rm -rf helloworld
+git clone https://github.com/fw876/helloworld && mv helloworld/luci-app-ssr-plus ./ && mv helloworld/shadowsocks-libev ./
+rm -rf helloworld
 ############################################################################################################################################################
 
 ########################################SSRP#############################################
-git clone https://github.com/zouchanggan/SSRP ./SSRP && mv SSRP/update/v2raya ./openwrt-passwall && mv SSRP/themes//luci-theme-design ./ && rm -rf SSRP
+git clone https://github.com/zouchanggan/SSRP ./SSRP && mv SSRP/update/v2raya ./ && mv SSRP/themes//luci-theme-design ./ && rm -rf SSRP
 ############################################################################################################################################################
 
 ########################################small#############################################
@@ -28,16 +30,16 @@ mv small/luci-app-homeproxy ./
 mv small/luci-app-mihomo ./ 
 mv small/luci-app-mosdns ./ 
 mv small/luci-app-openclash ./ 
-mv small/shadowsocks-rust ./openwrt-passwall 
-mv small/mosdns ./openwrt-passwall 
-mv small/v2ray-geodata ./openwrt-passwall
-mv small/chinadns-ng ./openwrt-passwall
-mv small/v2dat ./openwrt-passwall
-mv small/lua-neturl ./openwrt-passwall
-mv small/redsocks2 ./openwrt-passwall
-mv small/shadow-tls ./openwrt-passwall
-mv small/gn ./openwrt-passwall
-mv small/mihomo ./openwrt-passwall
+mv small/shadowsocks-rust ./
+mv small/mosdns ./ 
+mv small/v2ray-geodata ./
+mv small/chinadns-ng ./
+mv small/v2dat ./
+mv small/lua-neturl ./
+mv small/redsocks2 ./
+mv small/shadow-tls ./
+mv small/gn ./
+mv small/mihomo ./
 rm -rf small
 ############################################################################################################################################################
 
