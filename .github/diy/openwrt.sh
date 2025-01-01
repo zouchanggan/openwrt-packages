53f371484d48ba7c7016c844d96f3e3cf93e3212
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2025-01-01 21:27:14 +0800

    Update openwrt.sh

--- .github/diy/openwrt.sh
+++ .github/diy/openwrt.sh
@@ -16,7 +16,7 @@ git clone https://github.com/xiaorouji/openwrt-passwall ./passwall && mv passwal
 git clone https://github.com/xiaorouji/openwrt-passwall2 ./passwall2 && mv passwall2/luci-app-passwall2 ./ && rm -rf passwall2
 ########################################luci-app-ssr-plus#############################################
 #原作者
-git clone https://github.com/fw876/helloworld ./openwrt-ssr-plus && mv helloworld/luci-app-ssr-plus ./ && mv helloworld/shadowsocks-libev ./ && rm -rf openwrt-ssr-plus
+git clone https://github.com/fw876/helloworld ./openwrt-ssr-plus && mv helloworld/luci-app-ssr-plus ./ && mv helloworld/shadowsocks-libev ./ && rm -rf ./openwrt-ssr-plus
 ############################################################################################################################################################
 
 ########################################SSRP#############################################
