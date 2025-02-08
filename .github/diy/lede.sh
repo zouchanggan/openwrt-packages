a19f2066e7c0f13bda361d7e0ef32d244619c0c8
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2025-02-08 13:22:25 +0800

    Update lede.sh

--- .github/diy/lede.sh
+++ .github/diy/lede.sh
@@ -59,11 +59,9 @@ git clone https://github.com/sirpdboy/luci-app-autotimeset ./luci-app-autotimese
 ########################################luci-app-argon#############################################
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git ./luci-app-argon-config
-####################################################################################
-
 ########################################luci-theme-design#############################################
-git clone https://github.com/gngpp/luci-theme-design.git ./luci-theme-design
-git clone https://github.com/gngpp/luci-app-design-config.git ./luci-app-design-config
+git clone https://github.com/gngpp/luci-theme-design.git
+git clone https://github.com/gngpp/luci-app-design-config.git
 ####################################################################################
 
 
